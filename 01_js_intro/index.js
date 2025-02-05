d73bedd961d0550a696d261f0d04c9259b0c073f
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-02-05 15:22:50 +0530

    intro

---
+++ 01_js_intro/index.js
@@ -0,0 +1,31 @@
+// var prompt = require('prompt-sync')();
+
+// var num = prompt("enter")
+
+
+
+// console.log(num);
+
+var num; // declaration
+    num = 10; // assigning // defining
+
+
+    let some;
+
+    console.log(some)
+
+var num;
+    num = 20;
+
+let num2 = 20;
+
+num2 = 30;
+
+num2 = 100;
+
+
+const num3 = 500;
+
+
+
+console.log(num3)
