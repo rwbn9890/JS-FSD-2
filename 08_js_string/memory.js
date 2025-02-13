136298b3f7ee0a3bb5c88912a9e0b784611dfc46
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-02-13 15:26:11 +0530

    string, memory, data type

---
+++ 08_js_string/memory.js
@@ -0,0 +1,36 @@
+
+//     if(1=="1"){
+//         console.log('true')
+//     }
+//     else{
+//         console.log("false")
+//     }
+
+
+//  console.log(1=="1") // only value
+
+//  console.log(1==="1") // value + data type
+
+
+
+// console.log( null == 0); //
+// console.log( null > 0); // 0 > 0
+// console.log( null >= 0); // 0>=0
+
+let name = "alishan"
+
+let sirname = "shaikh"
+
+let username = name;
+
+let usen = name;
+
+
+let obj  = {
+     n : "amit",
+     sn : "dabhi"
+}
+
+
+let obj2 = obj;
+
