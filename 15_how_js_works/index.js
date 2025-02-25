d86401f260db6df2979ebec5ed1701f30f57ce11
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-02-25 15:16:21 +0530

    js works

---
+++ 15_how_js_works/index.js
@@ -0,0 +1,29 @@
+ 
+
+let val1 = 20;
+let val2 = 30;
+function squaren(n){
+    let result = n*n;
+    return result;
+}
+
+function fun(num1, num2){
+    let result = num1 + num2  
+    fun2();
+    return result;
+}
+
+function fun2(){
+    console.log("this isfun2 ")
+}
+
+
+
+
+let result1 = fun(val1, val2);
+let result2 = fun(50, 60);
+
+let result3 = square(5)
+
+
+
